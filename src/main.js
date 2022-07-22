c8a39ae6fbd403fa40a4c5fe31237cb2aa900fe3
Crisano97 <97keb6@gmail.com>
2022-07-22 17:57:39 +0200

    fixing main.js errors

--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faStar);
-Vue.component('font-awesome.icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 
 new Vue({
-  el:'#app',
-  components: { App },
-  template: <App/>,
+
   render: h => h(App),
 }).$mount('#app')
